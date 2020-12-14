2a9bc7ddbf10edb69c2041702330e837fe587303
Nhan Vi Nam <nhanvinamcoder@gmail.com>
2020-12-14 21:10:25 +0700

    Nam add member in group v1

--- components/ManagePeople/ModalAddFriendToGroup/index.js
+++ components/ManagePeople/ModalAddFriendToGroup/index.js
@@ -1,81 +1,98 @@
-import React from 'react';
-import { Modal, Button, Form, Select } from 'antd';
+import React, { useContext, useState, useCallback } from 'react';
+import { Modal, Button, Form, Checkbox } from 'antd';
 import Avatar from 'react-avatar';
 import { useSelector } from 'react-redux';
+import { InfoRoomContext } from 'components/common/context/InfoRoomContext';
 
 const prefix = 'modalAddFriendToGroup';
-const { Option } = Select;
 
 const ModalAddFriendToGroup = ({ ...props }) => {
   const { showModalAddFriendToGroup, handleCloseModalRoot } = props;
+  const [valueFriendAfterChecked, setValueFriendAfterChecked] = useState([]);
   const [form] = Form.useForm();
   const { listFriendContact } = useSelector(state => state.FriendReducer);
+  const { infoRoom } = useContext(InfoRoomContext);
 
-  const onFinish = values => {
-    console.log(values);
+  const onFinish = () => {
+    const list_user_id = [...valueFriendAfterChecked];
+    console.log(list_user_id);
   };
 
-  const renderOption = () => {
+  const findUserInRoom = useCallback(() => {
+    infoRoom?.users?.forEach(userInRoom => {
+      listFriendContact?.forEach(friend => {
+        if (userInRoom.id === friend.id) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+    });
+  }, [infoRoom?.users, listFriendContact]);
+
+  const renderInfoFriend = friendInfo => {
+    const checkAvatarFriend =
+      friendInfo.avatar === null || friendInfo.avatar === '';
+    return (
+      <>
+        <span className="friend--info">
+          {checkAvatarFriend ? (
+            <Avatar
+              size="50px"
+              className="avatar-create-group"
+              name={friendInfo.name}
+            />
+          ) : (
+            <img src={friendInfo.avatar} alt="avatar" />
+          )}
+          <span>{friendInfo.name}</span>
+        </span>
+      </>
+    );
+  };
+
+  const handleChangeCheckBox = valueCheckBox => {
+    setValueFriendAfterChecked(valueCheckBox);
+  };
+
+  const renderCheckBox = () => {
     return listFriendContact?.map(friend => {
       return (
-        <Option label={friend.name} value={friend.id} key={friend.id}>
-          <div className="select--info">
-            {friend.avatar === null || friend.avatar === '' ? (
-              <Avatar
-                size="38px"
-                className="avatar-create-group"
-                name={friend.name}
-              />
-            ) : (
-              <img src={friend.avatar} alt="avatar" />
-            )}
-            <span>{friend.name}</span>
-          </div>
-        </Option>
+        <Checkbox value={friend.id} key={friend.id} disabled={findUserInRoom}>
+          {renderInfoFriend(friend)}
+        </Checkbox>
       );
     });
   };
+
   return (
     <>
-      <>
-        <Modal
-          title="Thêm Bạn Vào Nhóm"
-          visible={showModalAddFriendToGroup}
-          onCancel={() => handleCloseModalRoot(false)}
-          footer={false}
-          className={prefix}
+      <Modal
+        title="Thêm Bạn Vào Nhóm"
+        visible={showModalAddFriendToGroup}
+        onCancel={() => handleCloseModalRoot(false)}
+        footer={false}
+        className={prefix}
+      >
+        <Form
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          form={form}
         >
-          <Form
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-            form={form}
-          >
-            <Form.Item name="list_user_id">
-              <Select
-                mode="multiple"
-                allowClear
-                style={{ width: '100%' }}
-                placeholder="Tìm kiếm bằng tên"
-                filterOption={(input, option) =>
-                  option?.label.toLowerCase().indexOf(input.toLowerCase()) >= 0
-                }
-              >
-                {renderOption()}
-              </Select>
-            </Form.Item>
+          <span>Có thể bạn quen biết</span>
+          <Form.Item>
+            <Checkbox.Group onChange={handleChangeCheckBox}>
+              {renderCheckBox()}
+            </Checkbox.Group>
+          </Form.Item>
 
-            <Form.Item>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{ width: '100%' }}
-              >
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
-        </Modal>
-      </>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </>
   );
 };
